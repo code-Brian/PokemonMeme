8a15d523f5d6523b3cf76f02e8ce8f365bf9bda8
Brian Denmark <testingarea479@gmail.com>
2022-07-30 14:35:18 -0700

    added basic wireframe components, will add more

--- script.js
+++ script.js
@@ -5,34 +5,36 @@ async function getPokemon(query) {
     // console.log(jsonData);
     let wrapper = document.querySelector('#responseWrapper')
     
-//     for(let i = 0; i < jsonData.results.length; i++) {
-//         let currentPokemon = await fetch(jsonData.results[i].url)
-//         let currentPokemonJSON = await currentPokemon.json()
+    for(let i = 0; i < jsonData.results.length; i++) {
+        let currentPokemon = await fetch(jsonData.results[i].url)
+        let currentPokemonJSON = await currentPokemon.json()
 
-//         console.log(currentPokemonJSON.name);
-//         for (let j = 0; j < currentPokemonJSON.abilities.length; j++) {
-//             console.log(currentPokemonJSON.abilities[j].ability.name);
-//         }
+        console.log(currentPokemonJSON.name);
+        for (let j = 0; j < currentPokemonJSON.abilities.length; j++) {
+            console.log(currentPokemonJSON.abilities[j].ability.name);
+        }
 
-//         let row = document.createElement('tr')
-//         let imageCell = document.createElement('td')
-//         let shinySprite = document.createElement('img')
-//         shinySprite.src = currentPokemonJSON.sprites.front_shiny
-//         imageCell.appendChild(shinySprite)
-//         row.appendChild(imageCell)
+        let row = document.createElement('tr')
+        let imageCell = document.createElement('td')
+        let shinySprite = document.createElement('img')
+        shinySprite.src = currentPokemonJSON.sprites.front_shiny
+        imageCell.appendChild(shinySprite)
+        row.appendChild(imageCell)
 
-//         let nameCell = document.createElement('td')
-//         nameCell.innerText = currentPokemonJSON.name
-//         row.appendChild(nameCell)
+        let nameCell = document.createElement('td')
+        let capitalizedName = `${currentPokemonJSON.name.charAt(0).toUpperCase()}${currentPokemonJSON.name.slice(1)}`
 
-//         let typeCell = document.createElement('td')
-//         typeCell.innerText = currentPokemonJSON.types[0].type.name
-//         row.appendChild(typeCell)
+        nameCell.innerText = capitalizedName
+        row.appendChild(nameCell)
+
+        let typeCell = document.createElement('td')
+        typeCell.innerText = currentPokemonJSON.types[0].type.name
+        row.appendChild(typeCell)
 
 
-//         wrapper.appendChild(row)
-//     }
-// }
+        wrapper.appendChild(row)
+    }
+}
 
 getPokemon('pokemon?limit=12')
 console.log('End of file')
